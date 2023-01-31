96209e64351445fc8a5abb131fa409e331d413dc
yl4467 <117980697+yl4467@users.noreply.github.com>
2023-01-31 20:48:13 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -308,6 +308,119 @@ class Dataset_elec(Dataset):
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
 
+
+class Dataset_airdelay(Dataset):
+    def __init__(self, data_set, root_path, flag='train', size=None,
+                 features='S', data_path='ETTh1.csv',
+                 target='OT', scale=True, inverse=False,timeenc=0, freq='h',  cols=None):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+        df_raw = df_raw.dropna(axis=0, how='any')
+
+        '''
+        df_raw.columns: ['date', ...(other features), target feature]
+        '''
+        cols = list(df_raw.columns)
+        #cols.remove(self.target)
+        #cols.remove('Cancelled')
+        #df_raw = df_raw[['date'] + cols + [self.target]]
+        # print(cols)
+        input_point = len(df_raw)
+        num_train = int(input_point *0.7)
+        num_test = int(input_point *0.2)
+        #num_vali = len(df_raw) - num_train - num_test
+        num_vali = int(input_point *0.1)
+        border1s = [0, num_train - self.seq_len, num_train + num_vali-self.seq_len]
+        border2s = [num_train, num_train + num_vali, num_train + num_vali + num_test]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.features == 'M' or self.features == 'MS':
+            #cols_data = df_raw.columns[2:]
+            df_data = df_raw[cols[2:]]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+        df_raw['arrTime'] = pd.DataFrame((df_raw['arrTime'].apply(int)).apply(str).str.zfill(4))
+        df_raw['arrTime'] = df_raw['arrTime'].replace('2400', '2359')
+        #print(len(df_raw['arrTime']))
+        tmp = df_raw['FlightDate'].add(' ').add(df_raw['arrTime'])
+        #print(tmp[1540:1542])
+        df_stamp = pd.to_datetime(tmp, format='%Y/%m/%d %H%M')
+        
+        #df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        #pd.to_datetime(df_raw['FlightDate'])
+        #df_stamp = pd.date_range(start='1/1/2022',periods=border2-border1, freq='M')
+        #df_stamp = df_raw[['date']][border1:border2]
+        #df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        #df_stamp['date'] = df_stamp
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            data_stamp = df_stamp.drop(['date'], 1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp.values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
+
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+        #self.data_stamp = data_stamp
+        
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)  
+    
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
                  features='S', data_path='ETTh1.csv', 
